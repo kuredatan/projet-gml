666fafa92a22cfb6636e7e7604e900b67220746a
Clémence Réda <reda.clmc@live.fr>
2018-12-19 17:50:03 +0100

    code

--- Code/benchmark.py
+++ Code/benchmark.py
@@ -8,9 +8,10 @@ import pandas as pd
 import numpy as np
 import random
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 # For replication
-#random.seed(123456789)
+random.seed(123456789)
 
 parser = argparse.ArgumentParser(description='Recommender system')
 parser.add_argument('--data', type=str, default='ml-1m', metavar='D',
@@ -18,7 +19,17 @@ parser.add_argument('--data', type=str, default='ml-1m', metavar='D',
 parser.add_argument('--user', type=int, default=1, metavar='U',
                     help='userID to which the object should be recommended.')
 parser.add_argument('--s', type=int, default=1, metavar='S',
-                    help='serendipity threshold for the recommendation.')
+                    help='serendipity threshold (>1) for the recommendation.')
+parser.add_argument('--K', type=int, default=5, metavar='K',
+                    help='candidate number for the [Lagrée et al.] recommendation.')
+parser.add_argument('--alpha', type=float, default=6, metavar='A',
+                    help='alpha parameter for the LinUCB recommendation.')
+parser.add_argument('--lambda_', type=float, default=1, metavar='L',
+                    help='lambda parameter for the LinUCB recommendation.')
+parser.add_argument('--epsilon', type=float, default=0.2, metavar='P',
+                    help='epsilon parameter (between 0 and 1) for the epsilon-greedy recommendation.')
+parser.add_argument('--N', type=int, default=10, metavar='N',
+                    help='time budget for the [Lagrée et al.] recommendation.')
 parser.add_argument('--eps', type=float, default=0.6, metavar='E',
                     help='epsilon threshold to build epsilon neighbourhood similarity graph.')
 parser.add_argument('--k', type=int, default=0, metavar='T',
@@ -63,8 +74,10 @@ features = np.loadtxt(fn, delimiter=',')
 
 switch = {
      "random": models.Random(X, W, features),
-     "lagree": models.Recommender(X, W, features, s=1, K=5, N=10),
-     "greedy": models.Greedy(X, W, features),
+     "lagree": models.Recommender(X, W, features, s=args.s, K=args.K, N=args.N),
+     "greedy": models.Greedy(X, W, features, epsilon=args.epsilon),
+     "linUCB": models.LinUCB(X, W, features, lambda_=args.lambda_, alpha=args.alpha),
+     "rotting": models.Rotting(X, W, features),
 }
 
 if (not switch.get(args.method, None)):
@@ -89,11 +102,56 @@ for _ in tqdm(range(n_iter), "Benchmark " + method_name):
 		print("Assertion error!")
 		nerr += 1
 		pass
-## To smooth the regret curve
-print("There have been " + str(nerr) + " errors.")
+## To smooth the regret/volume curve
+print("There have been " + str(nerr) + " errors/" + str(n_iter) + " iterations.")
 n_iter -= nerr
-regret *= 1/float(n_iter)
-recommender.regret_arr = regret.tolist()
-volume *= 1/float(n_iter)
-recommender.volume_arr = volume.tolist()
-recommender.plot_results()
+if (n_iter > 0):
+	regret *= 1/float(n_iter)
+	recommender.regret_arr = regret.tolist()
+	volume *= 1/float(n_iter)
+	recommender.volume_arr = volume.tolist()
+	recommender.plot_results()
+
+## Compare different values of serendipity threshold, ...
+if (method_name == "lagree"):
+	s_values = range(1, 6)
+	## TODO test K and N 
+	m = len(s_values)
+	regret = np.zeros((horizon, m))
+	volume = np.zeros((horizon, m))
+	colors = ["k","r","b","g","c","p","m"][:m+1]
+	for i in range(m):
+		recommender = models.Recommender(X, W, features, s=s_values[i], K=args.K, N=args.N)
+		nerr = 0
+		for _ in tqdm(range(n_iter), "Benchmark " + method_name + " s = " + str(s_values[i])):
+			recommender.reinitialize()
+			try:
+				for __ in range(horizon):
+					recommender.run(verbose=False)
+				regret[:, i] += np.array(recommender.regret_arr)
+				volume[:, i] += np.array(recommender.volume_arr)
+			except AssertionError:
+				print("Assertion error!")
+				nerr += 1
+				pass
+		## To smooth the regret/volume curve
+		print("There have been " + str(nerr) + " errors/" + str(n_iter) + " iterations.")
+		n_iter -= nerr
+		if (n_iter > 0):
+			regret[:, i] *= 1/float(n_iter)
+			volume[:, i] *= 1/float(n_iter)
+	plt.figure(1)
+	plt.title("Regret and diversity variation depending on the value of s parameter")
+	plt.subplot(121)
+	for i in range(m):
+		plt.plot(np.array(regret[:, i]).cumsum(), colors[i] + "-", label="Regret with s = " + str(s_values[i]))
+	plt.ylabel('(Expected) cumulative regret')
+	plt.xlabel('Rounds')
+	plt.legend()
+	plt.subplot(122)
+	for i in range(m):
+		plt.plot(np.array(volume[:, i]).cumsum(), colors[i] + "-", label="Volume with s = " + str(s_values[i]))
+	plt.ylabel('Diversity of the recommendations')
+	plt.xlabel('Rounds')
+	plt.legend()
+	plt.show()
--- Code/models.py
+++ Code/models.py
@@ -11,6 +11,10 @@ import scipy.spatial.distance as sd
 ## Recommenders    ##
 #####################
 
+########################################################################################
+########################################################################################
+## General model
+
 #' ratings: data frame of ratings: at least with the 
 # following columns ['UserID', 'MovieID', 'Rating'] each rating belongs to [|1,5|]
 #' features: feature NumPy matrix with which the graph was built
@@ -31,7 +35,7 @@ class Bandit(object):
 		## Binary function which indicates whether a node has been 
 		## explored by the user
 		self.n_masked = int(perc_masked*self.n_ratings)
-		self.masked_labels = list(map(str, sample(list(map(int, self.indexes)), n_masked)))
+		self.masked_labels = list(map(str, sample(list(map(int, self.indexes)), self.n_masked)))
 		for i in self.masked_labels:
 			assert i in self.indexes.values
 		self.f = pd.DataFrame(np.ones((1, self.n_obj)), columns=self.indexes)
@@ -61,6 +65,7 @@ class Bandit(object):
 	def plot_results(self):
 		n_iter = len(self.regret_arr)
 		plt.figure(1)
+		plt.title("Regret and diversity of the recommendations")
 		plt.subplot(121)
 		plt.plot(np.array(self.regret_arr).cumsum(), "r-", label="Regret")
 		plt.ylabel('(Expected) cumulative regret')
@@ -101,7 +106,7 @@ class Bandit(object):
 	def recommend(self):
 		pass
 
-	def update(self):
+	def update(self, reward, action):
 		pass
 
 	def run(self, verbose=True):
@@ -113,12 +118,14 @@ class Bandit(object):
 		self.regret_arr.append(regret)
 		volume = self.parallelotope_volume()
 		self.volume_arr.append(volume)
-		self.update()
+		self.update(reward, action)
+
+########################################################################################
+########################################################################################
 
 ## Random, uniform, recommendations			
 class Random(Bandit):
 	def recommend(self):
-		super(Random, self).recommend()
 		candidates = self.get_unexplored_items()
 		n = len(candidates)
 		assert n > 0, "All {} actions have been explored!".format(self.f.size)
@@ -126,23 +133,110 @@ class Random(Bandit):
 		action = np.random.choice(candidates, p=[1/float(n)]*n)
 		return action, best_reward
 
-## Greedy recommendations (maximize diversity of the recommendation)		
+########################################################################################
+########################################################################################
+
+## Greedy recommendations (maximize diversity of the recommendation)
+## - with probability 1-epsilon the action maximizing the diversity measure is selected
+## - with probability epsilon a random action is selected		
 class Greedy(Bandit):
+	def __init__(self, ratings, graph, features, epsilon, perc_masked=0.8, eps=[0,1]):
+		super(Greedy, self).__init__(ratings, graph, features, perc_masked=perc_masked, eps=eps)
+		self.epsilon = epsilon
+
 	def recommend(self):
-		super(Greedy, self).recommend()
 		candidates = self.get_unexplored_items()
 		n = len(candidates)
 		assert n > 0, "All {} actions have been explored!".format(self.f.size)
 		vol_ = np.zeros(n)
-		for i in range(n):
-			action = candidates[i]
-			self.f[action] = 1
-			v = self.parallelotope_volume()
-			vol_[i] = v
-			self.f[action] = 0
-		action = candidates[np.argmax(vol_)]
+		if (np.random.choice([0, 1], p=[self.epsilon, 1-self.epsilon])):
+			action = np.random.choice(candidates, p=[1/float(n)]*n)
+		else:
+			for i in range(n):
+				action = candidates[i]
+				self.f[action] = 1
+				v = self.parallelotope_volume()
+				vol_[i] = v
+				self.f[action] = 0
+			action = candidates[np.argmax(vol_)]
 		best_reward = self.best_reward()
 		return action, best_reward
+
+########################################################################################
+########################################################################################
+
+## Linear UCB
+## Code adapted from Matteo Pirotti for RL classes		
+class LinUCB(Bandit):
+	def __init__(self, ratings, graph, features, lambda_, alpha, random_state=0, noise=0., perc_masked=0.8, eps=[0,1]):
+		super(LinUCB, self).__init__(ratings, graph, features, perc_masked=perc_masked, eps=eps)
+		self.noise = noise
+		self.local_random = np.random.RandomState(random_state)
+		self.real_theta = self.local_random.randn(self.n_features)
+		self.theta_hat = np.zeros((self.n_features))
+		## Estimate of np.dot(Z_t^T, Z_t) = sum_{1 <= u <= t-1} np.dot(phi_{a_u}^T, phi_{a_u})
+		self.ZZ = np.zeros((self.n_features, self.n_features))
+		## Estimate of np.dot(Z_t^T, y_t) = sum_{1 <= u <= t-1} r_{u}*phi_{a_u}^T
+		self.Zy = np.zeros((self.n_features))
+		self.lambda_ = lambda_
+		self.I = np.eye(self.n_features)
+		self.alpha = alpha
+
+	def reward(self, action):
+		assert action in self.indexes.values, "{} not in the set of objects".format(action)
+		a = np.where(action == self.indexes.values)[0].tolist()[0]
+		reward = np.dot(self.features[a, :], self.real_theta) + self.noise * self.local_random.randn(1)[0]
+		return reward
+
+	def reinitialize(self):
+		super(LinUCB, self).reinitialize()
+		self.theta_hat = self.theta_hat*0+1
+		self.ZZ = self.ZZ*0
+		self.Zy = self.Zy*0
+
+	def update(self, reward, action):
+		a = np.where(action == self.indexes.values)[0].tolist()[0]
+		## Update on parameters
+		self.Zy += reward*self.features[a, :]
+		z_t = np.reshape(self.features[a, :], (1, self.n_features))
+		self.ZZ += np.multiply(np.transpose(z_t), z_t)
+		## Update lambda_ ? TODO
+		self.theta_hat = np.dot(np.linalg.inv(self.ZZ+self.lambda_*self.I), self.Zy)
+
+	def best_reward(self):
+		D = np.dot(self.features, self.real_theta)
+		return np.max(D)
+
+	def recommend(self):
+		n_a = self.n_actions
+		n_f = self.n_features
+		self.theta_hat = np.reshape(self.theta_hat, (n_f, 1))
+		phi = lambda a : np.reshape(self.features[a, :], (n_f, 1))
+		phiT = lambda a : np.transpose(phi(a))
+		est = np.linalg.inv(self.ZZ+self.lambda_*self.I)
+		beta = lambda a : self.alpha*np.sqrt(np.dot(phiT(a), np.dot(est, phi(a))))
+		arm_values = [np.dot(phiT(a), self.theta_hat)[0, 0]+beta(a)[0,0] for a in range(n_a)]
+		action = self.indexes.values[arm_values.index(max(arm_values))]
+		best_reward = self.best_reward()
+		return action, best_reward
+
+	@property
+	def n_features(self):
+		return self.features.shape[1]
+
+	@property
+	def n_actions(self):
+		return self.features.shape[0]
+
+########################################################################################
+########################################################################################
+
+## Rotting Bandit			
+class Rotting(Bandit):
+	pass
+
+########################################################################################
+########################################################################################
 
 ## Adaptation of [Lagrée et al.]'s method			
 class Recommender(Bandit):
@@ -150,78 +244,117 @@ class Recommender(Bandit):
 	#' K: number of candidates at each step
 	#' N: time budget
 	def __init__(self, ratings, graph, features, s, K, N, perc_masked=0.8, eps=[0,1]):
-		super(Recommender, self).__init__(ratings, graph, features, perc_masked=0.8, eps=[0,1])
+		super(Recommender, self).__init__(ratings, graph, features, perc_masked=perc_masked, eps=eps)
 		self.s = s
 		self.K = K
 		self.N = N
-	## K: number of "centroids" to select
 	## In practice, we perform K-means on the set of unexplored elements
 	## at distance less than s edges of explored elements
 	## We have ensured that the graph is connected in process-data.py
 	def select_candidates(self):
-		candidates = self.get_unexplored_items()
-		n = len(candidates)
+		unexplored = self.get_unexplored_items()
+		n = len(unexplored)
 		assert n > 0, "All {} actions have been explored!".format(self.f.size)
 		W = self.graph
 		W[W > 0] = 1
 		## Get elements at distance less than s of explored elements
 		## that is, min_{y explored} ||x-y||_{edges} <= s
-		W = np.linalg.matrix_power(W, self.s)
-		candidates = [np.where(c == self.indexes.values)[0].tolist()[0] for c in candidates]
+		## (W^s)_{i,j} gives the number of walks of length s between vertices i and j
+		## The number of walks of length at most s between i and j is thus
+		## sum_{k \leq s} (W^k)_{i,j}
+		WW = W
+		for k in range(1, self.s):
+			W = np.dot(W, W)
+			WW += W
+		W = np.asarray(WW > 0, dtype=int)
+		candidates = [np.where(c == self.indexes.values)[0].tolist()[0] for c in unexplored]
 		W = np.delete(W[candidates, :], candidates, 1)
 		candidates = np.array(candidates)[np.where(np.sum(W, 1) > 0)[0].tolist()].tolist()
 		## K-means + whiten?
 		## Use MiniBatchKMeans when |candidates| is too high
+		## Not using centroid graph with Floyd-Warshall TODO
 		F = self.features[candidates, :]
 		km = KMeans(n_clusters=self.K, random_state=0).fit(F)
 		centroids = km.cluster_centers_
 		dists = sd.squareform(sd.pdist(np.concatenate((F, centroids), 0), "sqeuclidean"))
-		dists = dists[-5:, :-5]
+		dists = dists[-self.K:, :-self.K]
 		## Select the node closest to each centroid
 		res = []
 		for i in range(self.K):
+			#print((F[np.argmin(dists[i, :]), :] == np.asarray(centroids[i,:] > 0.5, dtype=float)).all())
 			res.append(candidates[np.argmin(dists[i, :])])
 		candidates = [self.indexes.values[k] for k in res]
 		supports = []
+		## Support of unexplored items for each candidates
 		for k in res:
 			support = np.where(self.graph.values[k, :] == 1)[0].tolist()
 			support_k = []
 			for a in support:
 				id_a = self.indexes.values[a]
-				support_k.append([id_a, self.graph.values[k, a]])
+				## if unexplored
+				if (not self.f[id_a][0]):
+					#support_k.append([id_a, self.graph.values[k, a]])
+					support_k.append(id_a)
 			supports.append(support_k)
 		return candidates, supports
-
+	## TODO activated means reward > 3 (where max reward = 5, min reward = 1)
+	def is_active_node(self, reward):
+		return(int(reward > 3))
+	## The recommend will return the candidate with the 
+	## highest expected spread, as described in [Lagrée et al.]
 	def recommend(self):
-		super(Recommender, self).recommend()
-		candidates, supports = self.select_candidates(self.s, self.K)
+		candidates, supports = self.select_candidates()
+		## TODO Need to reinitialize because the setting changes
 		nk = np.ones(self.K)
-		sk = np.zeros((self.K, self.N))
+		## sk[k, t, i] = 1 iff. node i is activated by candidate k at time t
+		sk = np.zeros((self.K, self.N, self.n_obj))
+		## uk[k, i] = 1 iff. node i has been activated exactly once by candidate k
+		uk = np.zeros((self.K, self.n_obj))
+		## zk[k, i] = 1 iff. node i has never been activated by candidate k
+		zk = np.ones((self.K, self.n_obj))
+		## Good-Turing estimator
+		rk = np.zeros(self.K)
+		## Compute bk(t), as defined in [Lagrée et al.]
 		bk = np.zeros(self.K)
+		lbdk = np.zeros(self.K)
 		for t in range(self.K):
-			for k in candidates:
-				## TODO observe the spread for candidate k
-				pass
-			pass
+			for i_k in range(len(candidates)):
+				## Observe the "expected spread" for candidate k
+				#spread = [self.is_active_node(self.reward(s[0])) if (s[0] in supports[i_k]) else 0 for s in range(self.n_obj)]
+				spread = [self.is_active_node(self.reward(s)) if (s in supports[i_k]) else 0 for s in range(self.n_obj)]
+				sk[i_k, 0, :] = spread
+				for i in range(len(spread)):
+					if (spread[i]):
+						#idx = np.where(supports[i_k][i][0] == self.indexes.values)[0].tolist()[0]
+						idx = np.where(supports[i_k][i] == self.indexes.values)[0].tolist()[0]
+						if (not uk[i_k, idx]):
+							uk[i_k, idx] = 1
+						if (uk[i_k, idx]):
+							uk[i_k, idx] = 0
+						if (zk[i_k, idx]):
+							zk[i_k, idx] = 0
 		for t in range(self.K+1, self.N):
-			## TODO compute bk(t)
+			## Good-Turing estimator
+			for i_k in range(self.K):
+				hapaxes = [uk[i_k, i]*np.prod(zk[i_k, list(set([i]).symmetric_difference(range(self.n_obj)))]) for i in range(self.n_obj)]
+				rk[i_k] = 1/float(nk[i_k])*sum(hapaxes)
+				lbdk[i_k] = sum([np.sum(sk[i_k, s, :]) for s in range(int(nk[i_k]))])/float(nk[i_k])
+				## Compute bk(t), as defined in [Lagrée et al.]
+				bk[i_k] = rk[i_k]+(1+np.sqrt(2))*np.sqrt((lbdk[i_k]*np.log(4*t))/nk[i_k])+np.log(4*t)/float(3*nk[i_k])
 			## choose q = argmax bk(t)
-			## nk(q) += 1
-			## sk(q, t) = spread for q
-			pass
+			q = np.argmax(bk)
+			## Update
+			nk[q] += 1
+			#spread = [self.is_active_node(self.reward(s[0])) if (s[0] in supports[q]) else 0 for s in range(self.n_obj)]
+			spread = [self.is_active_node(self.reward(s)) if (s in supports[q]) else 0 for s in range(self.n_obj)]
+			sk[q, t, :] = spread
+		action = candidates[q]#candidates[np.argmax(bk)]
 		best_reward = self.best_reward(candidates)
 		return action, best_reward
 
-	def update(self):
-		## Update weights on graph edges
-		## TODO
-		pass
-		return action
-
 	def best_reward(self, candidates):
-		super(Recommender, self).best_reward()
 		for c in candidates:
-			assert c in self.indexes and not self.f[c][0], "{} not action".format(c)
+			assert c in self.indexes.values and not self.f[c][0], "{} not action".format(c)
 		r = np.matrix(self.R[self.indexes.isin(candidates)].Rating.values, dtype=int)[0]
 		best_reward = np.max(r)
 		return best_reward
